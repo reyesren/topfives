7cea3d7b0ace4a0dbdbe8b828327b43828b9af19
Renzo <r.reyes@live.com>
2020-12-27 13:23:28 -0500

    [develop] created new develop branch for development use

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+// adding this comment to be able to commit Develop branch
 function App() {
   return (
     <div className="App">

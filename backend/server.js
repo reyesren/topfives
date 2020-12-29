7e6d5e0181cc50b4410c8d190ded09f6e085a428
Michael Henry <michaeljhenry@hotmail.ca>
2020-12-28 20:40:39 -0500

    TOP16

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -10,5 +11,17 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
+app.use("/api/users", userRoutes);
 
 app.listen(5000, console.log("Listening on port 5000"));

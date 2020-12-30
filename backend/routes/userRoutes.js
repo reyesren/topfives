a3cc0d1cbf2fe93cc7b3de0aa22d179e56aa7d18
Michael Henry <michaeljhenry@hotmail.ca>
2020-12-29 22:28:31 -0500

    reverting user controller signup and db connection

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const usersController = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/:id", (req, res, next) =>
   res.json({ message: "success getting account" })
 );
-router.post("/signup", usersController.signup);
+router.post("/signup", (req, res, next) =>
+  res.json({ message: "success creating" })
+);
 router.post("/login", (req, res, next) =>
   res.json({ message: "success logging in" })
 );

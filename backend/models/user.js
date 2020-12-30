b1dfbfd943ac3c52be4c2403d0e9fce3f70c90cc
Michael Henry <michaeljhenry@hotmail.ca>
2020-12-30 02:28:05 -0500

    added database connection, dotenv, signup sends data to DB

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,8 +6,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
     image: { type: String },
     bio: { type: String },
     lists: [{ type: mongoose.Schema.Types.ObjectId, required: true }],

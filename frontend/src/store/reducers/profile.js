1c760711ee440ea2f0a3f52ef77e3ca2b7ad0c8a
Michael Henry <michaeljhenry@hotmail.ca>
2021-01-22 19:56:45 -0500

    TOP25. added actionTypes, action, reducer for userProfile page. added dynamic route for userPage

---
+++ frontend/src/store/reducers/profile.js
@@ -0,0 +1,48 @@
+import {
+  USER_PROFILE_REQUEST,
+  USER_PROFILE_SUCCESS,
+  USER_PROFILE_FAIL,
+} from "../actions/actionTypes";
+
+export const profileReducer = (
+  state = {
+    firstName: null,
+    lastName: null,
+    username: null,
+    email: null,
+    lists: [],
+    subscribers: null,
+    image: null,
+    loading: false,
+    error: null,
+  },
+  action
+) => {
+  switch (action.type) {
+    case USER_PROFILE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case USER_PROFILE_SUCCESS:
+      return {
+        ...state,
+        firstName: action.payload.firstName,
+        lastName: action.payload.lastName,
+        username: action.payload.username,
+        email: action.payload.email,
+        lists: action.payload.lists,
+        image: action.payload.image,
+        subscribers: action.payload.subscribers,
+        loading: false,
+      };
+    case USER_PROFILE_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};

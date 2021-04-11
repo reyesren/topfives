b729ee397c9f3937e92753e2851f277212c0bcff
Michael Henry <michaeljhenry@hotmail.ca>
2021-04-11 19:42:56 -0400

    reverting to previous

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("API is running..");
 });
 
 app.listen(5000, console.log("Listening on port 5000"));
